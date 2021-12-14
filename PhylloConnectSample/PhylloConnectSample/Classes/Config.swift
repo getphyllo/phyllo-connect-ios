34f30d99ed54033e32c3bc4152f2fa9df900f285
Pankaj Patel <patelpankaj.1010@gmail.com>
2021-12-14 11:05:31 +0530

    - cocoa pods removed and can be run with install pods

--- PhylloConnectSample/PhylloConnectSample/Classes/Config.swift
+++ PhylloConnectSample/PhylloConnectSample/Classes/Config.swift
@@ -11,13 +11,8 @@ import PhylloConnect
 
 struct Config {
     
-    //Dev
-    //static let client_id = "1ad05f8d-2fb1-44d0-896a-3ce509679b85"
-    //static let client_secret =  "b9f94c18-efde-4ab2-bb8a-6d47299a5fa9"
     static let client_id = "<client id will be here>"
     static let client_secret =  "<client secret will be here>"
     static let env = PhylloEnvironment.dev
-
 }
-
 

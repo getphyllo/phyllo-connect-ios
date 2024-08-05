1c0c094022684ca1e0f4a6aaebb3b58775d0c851
Pankaj Patel <patelpankaj.1010@gmail.com>
2024-08-05 16:04:55 +0530

    Release V0.3.6
    - fixes and improvement with crashes

--- PhylloConnectSample/PhylloConnectSample/Classes/Config.swift
+++ PhylloConnectSample/PhylloConnectSample/Classes/Config.swift
@@ -11,8 +11,8 @@ import PhylloConnect
 
 struct Config {
     
-    static var client_id = "<client id here>"
-    static var client_secret =  "<client secret here>"
+    static var client_id = "0a62ced6-3b00-4112-9c6b-7e7e61db02d0"
+    static var client_secret =  "6260ff9d-1026-4b75-8d81-b33e317b4581"
     static let env = PhylloEnvironment.sandbox
 }
 
